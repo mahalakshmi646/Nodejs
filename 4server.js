6b54a760c545a8ea18b7d9e28b60f650e56d9f83
mahalakshmi646 <pandrangi.mahalakshmi@gmail.com>
2024-12-30 16:06:59 +0530

    Add files via upload

---
+++ 4server.js
@@ -0,0 +1,6 @@
+const math = require('./3math');
+console.log(math.addition(45,65));
+console.log(math.subtraction(45,65));
+console.log(math.multiplication(45,65));
+console.log(math.division(45,65));
+console.log(math.remainder(45,65));
